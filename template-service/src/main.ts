f47f0ad7041372834066619b4032bd46896d50a8
luli-tech <olabodemicheal5@gmail.com>
2025-11-12 14:21:24 +0100

    Bring template-service folder from feature/templates

--- template-service/src/main.ts
+++ template-service/src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port);
-  Logger.log(
-    `Gateway service is running on: http://localhost:${port}/api`,
-    'Bootstrap',
-  );
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
