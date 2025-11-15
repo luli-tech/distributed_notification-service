fbd19ce46bad183789a83e3d3ddc706df1103f3c
luli-tech <olabodemicheal5@gmail.com>
2025-11-16 00:40:03 +0100

    fixed

--- push_service/src/modules/push.service.ts
+++ push_service/src/modules/push.service.ts
@@ -12,9 +12,9 @@ export class PushService implements OnModuleInit {
   onModuleInit() {
     const firebaseConfig = {
       projectId: this.configService.get<string>("FIREBASE_PROJECT_ID"),
-      privateKey: this.configService
-        .get<string>("FIREBASE_PRIVATE_KEY")
-        .replace(/\\n/g, "\n"),
+      privateKey: (
+        this.configService.get<string>("FIREBASE_PRIVATE_KEY") ?? ""
+      ).replace(/\\n/g, "\n"),
       clientEmail: this.configService.get<string>("FIREBASE_CLIENT_EMAIL"),
     };
 
