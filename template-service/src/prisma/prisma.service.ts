02041f909708bbfc0c34ed32a3a8aeb861e329e0
luli-tech <olabodemicheal5@gmail.com>
2025-11-15 21:10:21 +0100

    fixed

--- template-service/src/prisma/prisma.service.ts
+++ template-service/src/prisma/prisma.service.ts
@@ -1,15 +1,16 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-
+import { OnModuleDestroy, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma';
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
     await this.$connect();
+    console.log('Prisma connected to database');
   }
 
-  async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
-      await app.close();
-    });
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
