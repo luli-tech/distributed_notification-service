19f0c3c125ed6eea4e4f189d625ec88b99b83295
luli-tech <olabodemicheal5@gmail.com>
2025-11-15 20:49:38 +0100

    fixed

--- template-service/src/app.module.ts
+++ template-service/src/app.module.ts
@@ -3,9 +3,14 @@ import { ThrottlerModule } from '@nestjs/throttler';
 import { TemplateModule } from './template/template.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ThrottlerModule.forRoot({ ttl: 60, limit: 20 }), TemplateModule],
+  imports: [
+    ThrottlerModule.forRoot({ ttl: 60, limit: 20 }),
+    TemplateModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
